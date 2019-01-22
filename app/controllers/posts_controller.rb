3c8a6a47ea80fbf640270163dcde8dd12fceee20
Veniel Dorestave <veniel@noukod.com>
2019-01-22 18:07:57 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,27 @@ class PostsController < ApplicationController
 		@post = Post.find(params[:id])
 	end
 
+	def newclass PostsController < ApplicationController
+	def index
+		@posts = Post.all
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
 	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new
+		@post.title = params[:title]
+		@post.description = params[:description]
+		@post.save
+		redirect_to post_path(@post)
+	end
+end
 		@post = Post.new
 	end
 end
